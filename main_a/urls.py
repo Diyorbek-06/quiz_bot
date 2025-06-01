22e42f23bb443e9293fb1b4bc04b0cda8eed1284
Diyorbek-06 <diyorbekt146@gmail.com>
2025-06-01 23:49:09 +0500

    yangilandi

--- main_a/urls.py
+++ main_a/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import index
+from pkg_resources import parse_version
+
+from .views import index, ready_test
 from .user_wievs import signup
 urlpatterns = [
     path('', index, name='index'),
+    #test urls
+    path('<int:test_id', ready_test, name='ready_test'),
     #user urls
     path('signup', signup, name='signup'),
 ]
--- main_a/views.py
+++ main_a/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Test, Question
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    test = Test.objects.all()
+    return render(request, 'index.html', {'test':test})
+
+@login_required(login_url='login')
+def ready_test(request, test_id):
+    test = get_object_or_404(Test, id=test_id)
+    return render(request, 'ready_test', {'test':test})
+
+@login_required(login_url='login')
+def test(request, test_id):
+    test = get_object_or_404(Test, id=test_id)
+    question = Question.objects.filter(test=test)
+
+    contex = {'test': test, 'question':question}
+    return render(request, 'test.html', contex)
