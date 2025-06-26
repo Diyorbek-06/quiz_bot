be403c31fc8d99967442e8f526dbf8d656bd8cc3
Diyorbek-06 <diyorbekt146@gmail.com>
2025-06-26 08:23:39 +0500

    login qo'shildi

--- main_a/user_wievs.py
+++ main_a/user_wievs.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+
+@login_required(login_url='login')
+def profile(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    return render(request, 'login_html', {'user':user})
+
+
 
 def signup(request):
     form = UserCreationForm()
